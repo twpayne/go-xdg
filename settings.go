2ac768f1f303cba97d51cdc82aeac1bbdf58192a
Tom Payne <twpayne@gmail.com>
2019-02-12 23:45:10 +0100

    Add Settings

---
+++ settings.go
@@ -0,0 +1,65 @@
+package xdg
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+const (
+	settingsCmdName = "xdg-settings"
+
+	// DefaultURLSchemeHandlerProperty is the default URL scheme handler property.
+	DefaultURLSchemeHandlerProperty = "default-url-scheme-handler"
+
+	// DefaultWebBrowserProperty is the default web browser property.
+	DefaultWebBrowserProperty = "default-web-browser"
+)
+
+type settings struct{}
+
+// Settings checks, gets, and sets XDG settings. See
+// https://portland.freedesktop.org/doc/xdg-settings.html.
+var Settings settings
+
+// Check checks that value of property.subProperty is value.
+func (settings) Check(property, subProperty, value string) (bool, error) {
+	args := []string{"check", property}
+	if subProperty != "" {
+		args = append(args, subProperty)
+	}
+	args = append(args, value)
+	output, err := exec.Command(settingsCmdName, args...).Output()
+	if err != nil {
+		return false, err
+	}
+	s := strings.TrimSpace(string(output))
+	switch s {
+	case "yes":
+		return true, nil
+	case "no":
+		return false, nil
+	default:
+		return false, fmt.Errorf(`xdg.Settings.Check(%q, %q, %q): expected "yes" or "no", got %q`, property, subProperty, value, s)
+	}
+}
+
+// Get gets the value of property.subProperty.
+func (settings) Get(property, subProperty string) (string, error) {
+	args := []string{"get", property}
+	if subProperty != "" {
+		args = append(args, subProperty)
+	}
+	output, err := exec.Command(settingsCmdName, args...).Output()
+	return strings.TrimSpace(string(output)), err
+}
+
+// Set sets property.subProperty to value.
+func (settings) Set(property, subProperty, value string) error {
+	args := []string{"set", property}
+	if subProperty != "" {
+		args = append(args, subProperty)
+	}
+	args = append(args, value)
+	return exec.Command(settingsCmdName, args...).Run()
+}
